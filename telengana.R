8793fa943aba389b09acd044e57e94ebcac1f868
jayarajmadanu <jayarajmadanu@gmail.com>
2020-06-21 18:37:05 +0530

    daily cases

--- telengana.R
+++ telengana.R
@@ -2,10 +2,15 @@
 library(tidyverse)
 library(dslabs)
 
+## daily growth
 filter(complete,complete$Name.of.State...UT=="Telengana") %>% 
   ggplot(aes(Date,Total.Confirmed.cases))+geom_point()
 
-
+## total and avg cases per day
 filter(complete,complete$Name.of.State...UT=="Telengana") %>% 
   summarize(sum(New.cases,rm.na=T),mean(New.cases,rm.na=T))
 
+## daily cases
+filter(complete,complete$Name.of.State...UT=="Telengana") %>% 
+  select(c(Date,New.cases)) %>%
+  ggplot(aes(New.cases))+geom_histogram(binwidth = 5)
